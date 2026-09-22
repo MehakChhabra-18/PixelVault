4f326184086ec94c6c1c2b2b34569ffafbffd720
MehakChhabra-18 <mehakchhabra751@gmail.com>
2026-09-22 19:05:20 +0530

    Initialize PixelVault project

---
+++ README.md
@@ -0,0 +1,12 @@
+# PixelVault
+
+A C-Based Image Steganography and Steganalysis Toolkit.
+
+## Description
+
+PixelVault is a C project that explores hiding data inside
+digital images using steganography.
+
+## Current Status
+
+Milestone 1 - Project Foundation
---
+++ src/main.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(int argc, char*argv[])
+{
+    if(argc<2)
+    {
+        printf("PixelVault\n");
+        printf("A C-Based Image Stegnography & Steganalysis Toolkit\n\n");
+        
+        printf("Usage:\n");
+        printf(" pixelvault <command>\n\n");
+
+        printf("Commands:\n");
+        printf("  info      Display image information\n");
+        printf("  hide      Hide a message inside an image\n");
+        printf("  extract   Extract a hidden message\n");
+        printf("  capacity  Calculate image hiding capacity\n");
+        printf("  compare   Compare two images\n");
+        printf("  analyze   Analyze image properties\n");
+        printf("  detect    Perform basic steganalysis\n");
+
+        return 0;
+    }
+
+    if(strcmp(argv[1],"info")==0)
+    {
+        printf("INFO command selected.\n");
+    }
+    else if(strcmp(argv[1],"hide")==0)
+    {
+        printf("HIDE command selected.\n");
+    }
+    else if(strcmp(argv[1],"extract")==0)
+    {
+        printf("EXTRACT command selected.\n");
+    }
+    else if(strcmp(argv[1],"capacity")==0)
+    {
+        printf("CAPACITY command selected.\n");
+    }
+    else if(strcmp(argv[1],"compare")==0)
+    {
+        printf("COMPARE command selected.\n");
+    }
+    else if(strcmp(argv[1],"analyze")==0)
+    {
+        printf("ANALYZE command selected.\n");
+    }
+    else if(strcmp(argv[1],"detect")==0)
+    {
+        printf("DETECT command selected.\n");
+    }
+    else
+    {
+        printf("Unknown command: %s\n",argv[1]);
+        return 1;
+    }
+    return 0;
+}
